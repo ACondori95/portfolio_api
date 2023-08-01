68f1aa84f387ea09f05da7ff038ad9c0a51151f7
Alvaro Condori <con21033@byui.edu>
2023-08-01 01:01:30 -0300

    REST & JSON Alternatives

---
+++ routes/theme.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const themeController = require('../controllers/theme');
+
+router.get('/:themeName', themeController.getTheme);
+
+module.exports = router;
